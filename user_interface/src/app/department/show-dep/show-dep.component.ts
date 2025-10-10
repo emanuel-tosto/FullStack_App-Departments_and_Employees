0d47f0192757d13db9ba1618e750863629bf5800
Emanuel Tosto Holanda Vasconcelos <emanueltosto@gmail.com>
2025-10-09 22:59:13 -0300

    Creating the show-department screen, adding bootstrap and app-routing
    1) Creating the screen where the user can see all departments and make modifications in each one.
    2) adding bootstrap
    3) adding the routes for the Employee Component and Department Component

--- user_interface/src/app/department/show-dep/show-dep.component.ts
+++ user_interface/src/app/department/show-dep/show-dep.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-show-dep',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./show-dep.component.css']
 })
 export class ShowDepComponent {
+
+  constructor(private service:SharedService) {}
+
+  DepartmentList: any=[];
+  ngOnInit(): void{
+    this.refreshDepList();
+  }
+
+  refreshDepList(){
+    this.service.getDepList().subscribe(
+      data=>{this.DepartmentList=data;}
+    );
+  }
 
 }
