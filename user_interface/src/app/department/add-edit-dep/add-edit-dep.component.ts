4d48b3c4456b73eb6ed0d049dc0aed91f870e1e4
Emanuel Tosto Holanda Vasconcelos <emanueltosto@gmail.com>
2025-12-10 15:41:57 -0300

    adding delete and edit departments and employees, as modal pop-up windows
    1)Adding the Edit and Delete Department, Edit and Delete Employee
    Both are used as modal pop-up windows for better user experience.

--- user_interface/src/app/department/add-edit-dep/add-edit-dep.component.ts
+++ user_interface/src/app/department/add-edit-dep/add-edit-dep.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-add-edit-dep',
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-edit-dep.component.css']
 })
 export class AddEditDepComponent {
+
+  constructor() {}
+
+  @Input() dep:any;
+  DepartmentID:string;
+  DepartmentName:string;
+
+  ngOnInit():void {
+    this.DepartmentID=this.dep.DepartmentID;
+    this.DepartmentName=this.DepartmentName;
+  }
+
+  addDepartment() {
+    var val = {DepartmentID:this.DepartmentID, DepartmentName:this.DepartmentName};
+    this.service.addDepartment(val).subscribe(res=>{
+      alert(res.toString());
+    });
+  }
+
+  updateDepartment() {
+    var val = {DepartmentID:this.DepartmentID, DepartmentName:this.DepartmentName};
+    this.service.updateDepartment(val).subscribe(res=>{
+      alert(res.toString());
+    });
+
+  }
 
 }
--- user_interface/src/app/department/show-dep/show-dep.component.ts
+++ user_interface/src/app/department/show-dep/show-dep.component.ts
@@ -11,7 +11,43 @@ export class ShowDepComponent {
   constructor(private service:SharedService) {}
 
   DepartmentList: any=[];
+  ModalTitle:string;
+  ActivateAddEditDepComp:boolean=false;
+  dep:any;
   ngOnInit(): void{
+    this.refreshDepList();
+  }
+
+  addClick(){
+    this.dep={
+      DepartmentID:0,
+      DepartmentName:""
+    }
+    this.ModalTitle="Add Department";
+    this.ActivateAddEditDepComp=true;
+  }
+
+  editClick(){
+    this.dep=item;
+    this.ModalTitle="Edit Department";
+    this.ActivateAddEditDepComp=true;
+  }
+
+  deleteClick(item){
+    if (confirm("confirm deletion?")){
+      this.service.deleteDepartment(item.DepartmentID).subscribe(data=>{
+        alert(data.toString());
+        this.refreshDepList();
+      }
+
+      )
+    }
+
+
+  }
+
+  closeClick(){
+    this.ActivateAddEditDepComp=false;
     this.refreshDepList();
   }
 
